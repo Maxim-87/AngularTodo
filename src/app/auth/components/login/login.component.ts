9b7d365dd08417e45b78c01ddd2afcb5f645df70
Maxim-87 <maksboks87@mail.ru>
2023-02-20 10:44:47 +0600

    refactor login.component

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms'
+import {AuthService} from "../../../core/services/auth.service";
 
 @Component({
   selector: 'tl-auth',
@@ -6,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit{
-   constructor() {
-   }
+  loginForm: FormGroup<{ password: FormControl<string | null>; rememberMe: FormControl<boolean | null>; email: FormControl<string | null> }> = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    rememberMe: new FormControl(false)
+  })
 
-   ngOnInit() {
-   }
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    console.log(this.loginForm.value)
+    this.authService.login(this.loginForm.value)
+  }
+
+  get email() {
+    return this.loginForm.get('email')
+  }
+
+  get password() {
+    return this.loginForm.get('password')
+  }
 }
+
+
+
+
+
+
