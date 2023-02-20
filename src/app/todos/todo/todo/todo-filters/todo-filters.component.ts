1eab3410c77019bfba3846d293ff62bd6ea01259
Maxim-87 <maksboks87@mail.ru>
2023-02-20 10:45:55 +0600

    refactor todo-filters.component

---
+++ src/app/todos/todo/todo/todo-filters/todo-filters.component.ts
@@ -0,0 +1,23 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {FilterType} from "../../../models/todos.models";
+
+@Component({
+  selector: 'tl-todo-filters',
+  templateUrl: './todo-filters.component.html',
+  styleUrls: ['./todo-filters.component.css']
+})
+export class TodoFiltersComponent {
+  @Input() filter!: FilterType
+  @Output() changeFilterEvent = new EventEmitter<FilterType>()
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  changeFilterHandler(filter: FilterType) {
+    this.changeFilterEvent.emit(filter)
+  }
+}
