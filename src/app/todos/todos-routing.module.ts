18f0c30bdddaef05b83479886abc7d93812f59ac
Maxim-87 <maksboks87@mail.ru>
2023-04-11 13:06:36 +0600

    refactor todos-routing.module.ts

--- src/app/todos/todos-routing.module.ts
+++ src/app/todos/todos-routing.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {TodosComponent} from "./components/todos.component";
+import {AuthGuard} from "../core/guard/auth.guard";
 
 
-const routes: Routes = [{path: '', component: TodosComponent, pathMatch: "full"}]
+const routes: Routes = [{path: '', component: TodosComponent, pathMatch: "full", canActivate: [AuthGuard]}]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
